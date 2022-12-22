4a27e1fb634bf4dae7925588195c4888df8247f1
irsandahpr <irsandahpr@gmail.com>
2022-12-22 19:16:17 +0700

    Project UAS

---
+++ component/core/footer.php
@@ -0,0 +1,18 @@
+﻿ <?php
+ error_reporting(0);
+include 'configuration/config_connect.php';
+        $queryback="SELECT * FROM backset";
+		$resultback=mysqli_query($conn,$queryback);
+		$rowback=mysqli_fetch_assoc($resultback);
+		$footer=$rowback['footer'];
+
+
+ ?>
+ <footer class="main-footer">
+                <div class="pull-right hidden-xs">
+                    <b>Version</b> 1.5.0.1
+                </div>
+                <strong>Copyright © 2022 E - GLOWASH LAUNDRY</strong> All rights
+                reserved.
+				</div>
+            </footer>
